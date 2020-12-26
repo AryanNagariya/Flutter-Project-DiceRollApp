7d9d089ebc778167f048d3292c096d15a801e0d0
AryanNagariya <aryannagariya@gmail.com>
2020-12-25 23:16:19 -0800

    Independent Function

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,6 @@ class _DicePageState extends State<DicePage> {
                 setState(() {
                   print('Left Button is pressed');
                   leftDiceNumber = Random().nextInt(6) + 1;
-                  rightDiceNumber = Random().nextInt(6) + 1;
                 });
               },
             ),
@@ -48,7 +47,6 @@ class _DicePageState extends State<DicePage> {
                 setState(() {
                   print('Right Button is pressed');
                   rightDiceNumber = Random().nextInt(6) + 1;
-                  leftDiceNumber = Random().nextInt(6) + 1;
                 });
               },
             ),
